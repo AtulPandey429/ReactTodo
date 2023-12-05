37c82e83813ec198a99b95ad2267b94723d778fe
Atul Pandey <101567257+AtulPandey429@users.noreply.github.com>
2023-12-05 23:32:59 +0530

    added session storage and bootstrap

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,41 +1,66 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const Todo = () => {
-   const [todo, settodo] = useState([]);
-   const [newtodo, setnewtodo] = useState('');
+    const initialtodo =JSON.parse(sessionStorage.getItem('todo')) || [];
+  const [todo, settodo] = useState(initialtodo);
+  const [newtodo, setnewtodo] = useState("");
 
-   const handleAdd =()=>{
-    if(newtodo.trim()!=''){
-    settodo([newtodo,...todo]);
-    console.log(todo);
-    setnewtodo('')
-}
-   }
-const handleinput=(e)=>{
-    setnewtodo(e.target.value)
-}
-const handledelet=(index)=>{
-    const updatedtodo = todo.filter((_,i)=> i !== index);
+  const handleAdd = () => {
+    if (newtodo.trim() != "") {
+      settodo([newtodo, ...todo]);
+      console.log(todo);
+      setnewtodo("");
+    }
+  };
+  const handleinput = (e) => {
+    setnewtodo(e.target.value);
+  };
+  const handledelet = (index) => {
+    const updatedtodo = todo.filter((_, i) => i !== index);
     settodo(updatedtodo);
-    console.log(todo)
-}
+    console.log(todo);
+  };
+ 
+  //store
+  useEffect(() => {
+    sessionStorage.setItem('todo', JSON.stringify(todo));
+  }, [todo]);
 
-  return <div>
-    
-    <h1>Todo-App</h1>
-    <div className="container">
-        <input type="text" name="todo" value={newtodo} id="todo" onChange={handleinput} />
-        <button onClick={handleAdd}>Add</button>
+  return (
+    <div>
+      <h1>Todo-App</h1>
+      <div className="container">
+        <input
+          type="text"
+          name="todo"
+          value={newtodo}
+          id="todo"
+          onChange={handleinput}
+        />
+        <button className="btn m-2 btn-success" onClick={handleAdd}>
+          Add
+        </button>
+      </div>
+      <div className="container1">
+        {todo.map((e, i) => {
+          return (
+            <div className="row" key={i}>
+              <div className="col m-2 text-start ">{e} </div>
+              <div className="col m-2 ">
+                <button
+                  className=" btn btn-danger"
+                  onClick={() => handledelet(i)}
+                >
+                  delete
+                </button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
-    <div className="container1">
-     {todo.map((e,i)=>{
-        return <li key={i}>{e} 
-        <button onClick={()=>handledelet(i)} >delete</button>
-        </li>   })}
-    </div>
-
-
-  </div>;
+  );
 };
 
 export default Todo;
