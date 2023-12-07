d3385e2fac430ef663c4bec6f6b19072eafe7e96
Atul Pandey <101567257+AtulPandey429@users.noreply.github.com>
2023-12-08 00:07:25 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Todo from "./components/todo";
-
+import Topics from "./components/Topics";
+// import "./components/global.css"
 
 function App() {
   const [count, setCount] = useState(0);
@@ -11,8 +12,8 @@ function App() {
   return (
     <>
     <div className="parent">
-
-      <Todo/>
+      <Topics/>
+      {/* <Todo/> */}
     </div>
     </>
   );
