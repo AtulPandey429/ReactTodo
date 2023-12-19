3da9da7293135e2f3098fb8c52c55fc523581d47
Atul Pandey <101567257+AtulPandey429@users.noreply.github.com>
2023-12-19 12:32:08 +0530

    api integration

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -3,20 +3,38 @@ import { FaEdit, FaFilter, FaSave, FaTrash } from "react-icons/fa";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import "./Todo.css";
+
 const Todo = () => {
-  const initialTodo = JSON.parse(sessionStorage.getItem("todo")) || [];
-  const initialProgress = JSON.parse(sessionStorage.getItem("progress")) || [];
-  const [todo, setTodo] = useState(initialTodo);
+  const [todos, setTodos] = useState([]);
   const [newTodo, setNewTodo] = useState("");
   const [editIndex, setEditIndex] = useState(-1);
   const [editMode, setEditMode] = useState(false);
-  const [progress, setProgress] = useState(initialProgress);
   const [filter, setFilter] = useState('all');
+
+  useEffect(() => {
+    fetchTodoData();
+  }, []); // Fetch data when the component mounts
+
+  const fetchTodoData = async () => {
+    try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/todos'); // Replace 'https://your-api-url' with your API endpoint
+      const data = await response.json();
+
+      const extractedTodos = data.map(item => ({
+        id: item.id,
+        taskName: item.title,
+        completed: item.completed
+      }));
+
+      setTodos(extractedTodos);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
 
   const handleAdd = () => {
     if (newTodo.trim() !== "") {
-      setTodo([newTodo, ...todo]);
-      setProgress([false, ...progress]);
+      setTodos([{ id: Date.now(), taskName: newTodo, completed: false }, ...todos]);
       setNewTodo("");
     }
   };
@@ -25,70 +43,69 @@ const Todo = () => {
     setNewTodo(e.target.value);
   };
 
-  const handleDelete = (index) => {
-    const updatedTodo = todo.filter((_, i) => i !== index);
-    const updatedProgress = progress.filter((_, i) => i !== index);
-    setTodo(updatedTodo);
-    setProgress(updatedProgress);
-  };
-
   const handleEdit = (index) => {
     setEditIndex(index);
     setEditMode(true);
-    setNewTodo(todo[index]);
+    setNewTodo(todos[index].taskName);
   };
 
   const handleSave = () => {
     if (editIndex !== -1 && newTodo.trim() !== "") {
-      const updatedTodo = [...todo];
-      updatedTodo[editIndex] = newTodo;
-      setTodo(updatedTodo);
+      const updatedTodos = [...todos];
+      updatedTodos[editIndex].taskName = newTodo;
+      setTodos(updatedTodos);
       setEditIndex(-1);
       setEditMode(false);
       setNewTodo("");
     }
   };
 
+  const handleDelete = (id) => {
+    const updatedTodos = todos.filter(todo => todo.id !== id);
+    setTodos(updatedTodos);
+    setEditIndex(-1);
+    setEditMode(false);
+  };
+
   useEffect(() => {
-    sessionStorage.setItem("todo", JSON.stringify(todo));
-    sessionStorage.setItem("progress", JSON.stringify(progress));
-  }, [todo, progress]);
+    sessionStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   const filteredTodo = () => {
     if (filter === 'done') {
-      return todo.filter((_, index) => progress[index]);
+      return todos.filter(task => task.completed);
     } else if (filter === 'inProgress') {
-      return todo.filter((_, index) => !progress[index]);
+      return todos.filter(task => !task.completed);
     }
-    return todo;
+    return todos;
   };
 
   return (
     <center>
-    <div className="container  ">
-      <h1 className="text-center mb-5">Todo App</h1>
-      <div className="input-group mb-3">
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Enter a task"
-          value={newTodo}
-          onChange={handleInput}
-        />
-        <button className="btn btn-primary" onClick={handleAdd}>
-          Add Task
+      <div className="container">
+        <h1 className="text-center mb-5">Todo App</h1>
+        <div className="input-group mb-3">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Enter a task"
+            value={newTodo}
+            onChange={handleInput}
+          />
+          <button className="btn btn-primary" onClick={handleAdd}>
+            Add Task
+          </button>
+        </div>
+        <div className="dropdown mb-3">
+        <button
+          className="btn btn-secondary dropdown-toggle"
+          type="button"
+          id="dropdownMenuButton"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+        >
+          <FaFilter className="me-2" /> Filter Tasks
         </button>
-      </div>
-      <div className="dropdown mb-3">
-      <button
-    className="btn btn-secondary dropdown-toggle"
-    type="button"
-    id="dropdownMenuButton"
-    data-bs-toggle="dropdown"
-    aria-expanded="false"
-  >
-    <FaFilter className="me-2" /> Filter Tasks
-  </button>
         <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
           <li>
             <button
@@ -116,68 +133,68 @@ const Todo = () => {
           </li>
         </ul>
       </div>
-      <table className="table table-striped">
-        <thead>
-          <tr>
-            <th>Task Name</th>
-            <th>Edit/Save</th>
-            <th>Delete</th>
-            <th>Progress</th>
-          </tr>
-        </thead>
-        <tbody>
-          {filteredTodo().map((task, index) => (
-            <tr key={index}>
-              <td>
-                {editIndex === index && editMode ? (
-                  <input
-                    type="text"
-                    value={newTodo}
-                    onChange={handleInput}
-                    className="form-control"
-                  />
-                ) : (
-                  task
-                )}
-              </td>
-              <td>
-                {editIndex === index && editMode ? (
-                  <button className="btn btn-danger" onClick={handleSave}>
-                    <FaSave /> Save
-                  </button>
-                ) : (
-                  <button
-                    className="btn btn-warning"
-                    onClick={() => handleEdit(index)}
-                  >
-                    <FaEdit /> Edit
-                  </button>
-                )}
-              </td>
-              <td>
-                <button
-                  className="btn btn-danger"
-                  onClick={() => handleDelete(index)}
-                >
-                  <FaTrash /> Delete
-                </button>
-              </td>
-              <td>
-                <input
-                  type="checkbox"
-                  checked={progress[index]}
-                  onChange={() => {
-                    const newProgress = [...progress];
-                    newProgress[index] = !newProgress[index];
-                    setProgress(newProgress);
-                  }}
-                />
-              </td>
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th>Task Name</th>
+              <th>Edit/Save</th>
+              <th>Delete</th>
+              <th>Progress</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+          </thead>
+          <tbody>
+            {filteredTodo().map((task, index) => (
+              <tr key={task.id}>
+                <td>
+                  {editIndex === index && editMode ? (
+                    <input
+                      type="text"
+                      value={newTodo}
+                      onChange={handleInput}
+                      className="form-control"
+                    />
+                  ) : (
+                    <span >{task.taskName}</span>
+                  )}
+                </td>
+                <td>
+                  {editIndex === index && editMode ? (
+                    <button className="btn btn-danger" onClick={handleSave}>
+                      <FaSave /> Save
+                    </button>
+                  ) : (
+                    <button
+                      className="btn btn-warning"
+                      onClick={() => handleEdit(index)}
+                    >
+                      <FaEdit /> Edit
+                    </button>
+                  )}
+                </td>
+                <td>
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => handleDelete(task.id)}
+                  >
+                    <FaTrash /> Delete
+                  </button>
+                </td>
+                <td>
+                  <input
+                    type="checkbox"
+                    checked={task.completed}
+                    onChange={() => {
+                      const updatedTodos = [...todos];
+                      updatedTodos[index].completed = !task.completed;
+                      setTodos(updatedTodos);
+                    }}
+                  />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </center>
   );
 };
